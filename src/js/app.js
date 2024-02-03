00061d4ee01eca0c768559805927bcf68f91c219
ZavyalovAndrei <zaver1983@google.com>
2024-02-03 21:28:36 +0100

    Fix app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import GameSavingLoader from "./GameSavingLoader.js";
 
-try {
-  const saving = await GameSavingLoader.load();
-  console.log(saving);
-} catch (error) {
-  console.log(error);
-}
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    console.log(saving);
+  } catch (error) {
+    console.log(error);
+  }
+})();
